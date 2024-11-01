12f39da806fff6f09349bf1810079eac1b163d0e
Martin Olsen <Martin.Olsen2@protonmail.com>
2024-10-31 20:20:06 -0500

    Modelled Problem in Type System

--- Main.hs
+++ Main.hs
@@ -1,6 +1,31 @@
 module Main (main) where
 
 import Control.Monad (forever, when)
+import System.IO
+  ( BufferMode (NoBuffering),
+    hSetBuffering,
+    stdout,
+  )
 
 main :: IO ()
-main = undefined
+main = do
+  hSetBuffering stdout NoBuffering
+  hall <- hallMaker
+  runGame hall
+
+data Hall = Hall Selection Door Door Door
+
+newtype Selection = Selection Int
+  deriving (Eq)
+
+makeSelection :: Integral a => a -> Maybe Selection
+makeSelection n
+  | n < 0 = Nothing
+  | n <= 2 = Just $ Selection $ fromIntegral n
+  | otherwise = Nothing
+
+data Door = Door Revealed Item
+
+type Revealed = Bool
+
+data Item = Car | Goat
