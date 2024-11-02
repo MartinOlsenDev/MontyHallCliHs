5698d9187b9383143391c2deee63f0d2d7dc3f80
Martin Olsen <Martin.Olsen2@protonmail.com>
2024-11-02 17:31:59 -0500

    Wrote Basic State Transitions for Model

--- Main.hs
+++ Main.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main (main) where
 
 import Control.Monad (forever, when)
+import Data.Maybe
 import System.IO
   ( BufferMode (NoBuffering),
     hSetBuffering,
     stdout,
   )
+import System.Random (randomRIO)
 
 main :: IO ()
 main = do
@@ -16,21 +20,58 @@ main = do
 -- Plan to use a (+1) | (+2) in (mod 3) to form an infallible model
 -- Also, consider not storing which door is revealed in a data type at all;
 -- instead, consider only ever calculating it on the spot
-data Hall = Hall (Maybe (PlayerSelection, Maybe Int)) WinningDoor
+data Phase1 doorId = Phase1 doorId
 
-type PlayerSelection = Selection
+data Phase2 doorId = Phase2 doorId Winning RevealedDoorNext
 
-type WinningDoor = Selection
+data Phase3 doorId = Phase3 doorId Winning doorId
 
-type RevealedSelection = Selection
+createPhase1 :: IO (Phase1 DoorId)
+createPhase1 = do
+  randomDoor <- curry randomRIO 0 2
+  return $ Phase1 $ DoorId randomDoor
 
-newtype Selection = Selection Int
-  deriving (Eq)
+advance1 :: Integral a => Phase1 a -> a -> IO (Phase2 a)
+advance1 (Phase1 winDoor) choice = do
+  if winDoor == choice
+    then do
+      randomDoor <- curry randomRIO 0 1
+      return $ Phase2 winDoor True (randomDoor == 1)
+    else
+      let unusedDoor = fromJust . newFinder [winDoor, choice] $ enumFromTo 0 2
+          isNext = (winDoor + 1) `mod` 3 == unusedDoor
+       in return $ Phase2 winDoor False isNext
 
-makeSelection :: Integral a => a -> Maybe Selection
+advance2 :: Integral a => Phase2 a -> Bool -> Phase3 a
+advance2 (Phase2 winDoor isWinning isNext) switched =
+  let nowWinning = logicalXor isWinning switched
+      revealedDoor =
+        if isNext
+          then fromIntegral $ (winDoor + 1) `mod` 3
+          else fromIntegral $ (winDoor + 2) `mod` 3
+      prevLoc =
+        if isWinning
+          then winDoor
+          else fromJust $ newFinder [winDoor, revealedDoor] $ enumFromTo 0 2
+   in Phase3 winDoor nowWinning prevLoc
+
+type Winning = Bool
+
+type RevealedDoorNext = Bool
+
+type PlayerSelection = DoorId
+
+type WinningDoor = DoorId
+
+type RevealedSelection = DoorId
+
+newtype DoorId = DoorId Integer
+  deriving (Eq, Integral, Real, Ord, Num, Enum)
+
+makeSelection :: Integral a => a -> Maybe DoorId
 makeSelection n
   | n < 0 = Nothing
-  | n <= 2 = Just $ Selection $ fromIntegral n
+  | n <= 2 = Just $ DoorId $ fromIntegral n
   | otherwise = Nothing
 
 data Item = Car | Goat
@@ -41,6 +82,14 @@ instance Show Item where
      in case x of
           Car -> go "car"
           Goat -> go "goat"
+
+-- given two lists, find the only member of the second not
+-- occuring in the first
+newFinder :: Eq a => [a] -> [a] -> Maybe a
+newFinder a = find (\x -> not . elem x a)
+
+logicalXor :: Bool -> Bool -> Bool
+logicalXor a b = (a || b) && not (a && b)
 
 hallMaker = undefined
 
